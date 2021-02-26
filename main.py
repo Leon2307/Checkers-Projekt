13f12ce74372fdbb4eb3feac8e18eca1c88576df
Leon <leonfischer20031@web.de>
2021-02-26 22:26:56 +0100

    Main Grundstein

---
+++ main.py
@@ -0,0 +1,52 @@
+import feld
+import gui
+
+SPIELFELDGROESSE = 8
+
+def startFeld():
+    spielfeld = []
+    for y in range(SPIELFELDGROESSE):
+        spielfeld.append([])
+        for x in range(SPIELFELDGROESSE):
+            spielfeld[y].append(feld.Feld())
+
+            #Farbe des Feldes
+            if (y + x) % 2 == 0:
+                spielfeld[y][x].makeBlack(True)
+            else:
+                spielfeld[y][x].makeWhite(True)
+
+            #Steine belegung
+            if y < 3 and spielfeld[y][x].isBlack():
+                spielfeld[y][x].makeComputer(True)
+            elif y > 4 and spielfeld[y][x].isBlack():
+                spielfeld[y][x].makePlayer(True)
+
+    return spielfeld
+
+def main():
+    feld = startFeld()
+
+    gewonnen = False
+    momSpieler = True #Momentaner Spieler => spieler = True; computer = False
+
+    #Dauerschleife
+    while True:
+
+        #Solange das Spiel läuft
+        while not gewonnen:
+            if momSpieler:
+                x, y = spielerZug()
+                feld[y][x].makePlayer(True)
+            else:
+                x, y = computerZug()
+                feld[y][x].makeComputer(True)
+
+            gui.draw()
+
+        #Wenn das Spiel beendet ist
+        while gewonnen:
+            return
+
+if __name__ == "__main__":
+    main()
